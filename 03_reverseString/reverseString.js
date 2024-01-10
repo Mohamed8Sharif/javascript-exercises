bda4ad5607264a0a7114abedc27eeba3828ac50e
Mohamed Sharif <Mohamed8sharif@gmail.com>
2024-01-09 22:13:38 -0600

    complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(reversedString) {
+    let tempString = "";
+    
+    // Loops through the string backwards and concats its to the temp one
+    for(let i = reversedString.length-1; i >= 0; i--)
+    {
+        // NOTE TO REMEMBER .concat returns the value! It doesnt modify the string your acting upon!
+        tempString = tempString.concat(reversedString.charAt(i));
+    }
 
+    return tempString;
 };
 
 // Do not edit below this line
