77520fc6b8cd4c53388c9cb4b89f918d9a13cc40
Mohamed Sharif <Mohamed8sharif@gmail.com>
2024-01-09 23:00:40 -0600

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    if(typeof(firstNum) != 'number' || typeof(secondNum) != 'number' || firstNum < 0 || secondNum < 0)
+    {
+        return "ERROR"
+    }
+
+    let tempValue = 0;
+
+    if(firstNum < secondNum)
+    {
+        for(let i = firstNum; i <= secondNum; i++)
+        {
+            tempValue += i;
+        }
+    }
+    else
+    {
+        for(let i = secondNum; i <= firstNum; i++)
+        {
+            tempValue += i;
+        }
+    }
+
+    return tempValue;
 
 };
 
