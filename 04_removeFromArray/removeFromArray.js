ac8d51e4c5ebecd129929317789340978b252cee
Mohamed Sharif <Mohamed8sharif@gmail.com>
2024-01-12 23:13:06 -0600

    I dont like this exercise :p

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let newArray = args[0];
 
+    for(let i = 1; i < args.length; i++)
+    {
+        for(let j = newArray.length-1; j >= 0; j++)
+        {
+            if(args[i] == newArray[j])
+            {
+                newArray.splice(j,1);
+            }
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
